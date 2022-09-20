fbeedf79edf65aa1a47be99c9ba90c039a13c2ae
Lucasfth <luha@itu.dk>
2022-09-20 15:53:36 +0200

    add Extensions and Tests

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -3,8 +3,57 @@ namespace Assignment2.Tests;
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void FlattensArray()
     {
+        // Arrange
+        int[] array123 = { 1, 2, 3 };
+        int[] array456 = { 4, 5, 6 };
 
+        IEnumerable<int>[] xs = { array123, array456 };
+        int[] expected = { 1, 2, 3, 4, 5, 6 };
+
+        // Act
+        var actual = xs.Flatten();
+
+        // Assert
+        Assert.Equal(actual, expected);
+    }
+
+    [Fact]
+    public void FilterArrayForIntWhichDivisibleBy7AndGreaterThan42()
+    {
+        // Arrange
+        int[] ys = { 21, 36, 42, 12, 49, 56 };
+        int[] expected = { 49, 56 };
+
+        Predicate<int> divisibleBy7 = DivisibleBy7;
+        Predicate<int> greaterThan42 = GreaterThan42;
+
+        bool DivisibleBy7(int i) => i % 7 == 0;
+        bool GreaterThan42(int i) => i > 42;
+
+        // Act
+        var actual = ys.Filter(divisibleBy7).Filter(greaterThan42);
+        
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void FilterArrayForIntWhichIsLeapYear()
+    {
+        // Arrange
+        int[] ys = { 2020, 2000, 1600, 1900, 1800, 1582 };
+        int[] expected = { 2000, 1600 };
+
+        Predicate<int> isLeapYear = IsLeapYear;
+
+        bool IsLeapYear(int i) => 1582 < i && i % 4 == 0 && i % 100 == 0 && i % 400 == 0;
+
+        // Act
+        var actual = ys.Filter(isLeapYear);
+        
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,4 +2,25 @@ namespace Assignment2;
 
 public static class Extensions
 {
+    public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items)
+    {
+        foreach (var outer in items)
+        {
+            foreach (var inner in outer)
+            {
+                yield return inner;
+            }
+        }
+    }
+    
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate)
+    {
+        foreach (var item in items)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
 }
