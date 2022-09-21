7ac12bdab0fe36eead6f1a60a4da706059ba4d9a
Mathias Hvolgaard Darling Larsen <mhvl@itu.dk>
2022-09-21 15:18:27 +0200

    IsSecure and WordCount implemented and tested

--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -1,5 +1,10 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment2;
 
 public static class Extensions
 {
+    public static bool IsSecure(this Uri url) => url.Scheme == "https";
+
+    public static int WordCount(this string sentence) => Regex.Matches(sentence, @"[a-zA-Z]+").Count;
 }
--- StudentLibrary.Tests/StudentTests.cs
+++ StudentLibrary.Tests/StudentTests.cs
@@ -13,7 +13,7 @@ public class StudentTests
         var actual = s.Status;
         
         // Assert
-        expected.Should().Be(actual);
+        actual.Should().Be(expected);
     }
 
     [Fact]
@@ -23,11 +23,11 @@ public class StudentTests
         Student s = new Student { Id = 1, GivenName = "Byggemand", SurName = "Bob", StartDate = new DateTime(2022, 09, 8), EndDate = new DateTime(2022, 12, 30), GraduationDate = new DateTime(2022, 12, 30) };
         
         //Act
-        var expectedStatus = Status.Active; 
-        var actualStatus = s.Status;
+        var expected = Status.Active; 
+        var actual = s.Status;
 
         //Assert
-        expectedStatus.Should().Be(actualStatus);
+        actual.Should().Be(expected);
 
     }
 
@@ -38,12 +38,11 @@ public class StudentTests
         Student s = new Student { Id = 1, GivenName = "Karl", SurName = "Karlsen", StartDate = new DateTime(2021, 09, 19), EndDate = new DateTime(2022, 6, 30), GraduationDate = new DateTime(2023, 12, 30) };
         
         //Act
-        var expectedStatus = Status.Dropout; 
-        var actualStatus = s.Status;
+        var expected = Status.Dropout; 
+        var actual = s.Status;
 
         //Assert
-        expectedStatus.Should().Be(actualStatus);
-
+        actual.Should().Be(expected);
     }
 
     [Fact]
@@ -53,12 +52,11 @@ public class StudentTests
         Student s = new Student { Id = 1, GivenName = "Sofie", SurName = "Sofiesen", StartDate = new DateTime(2020, 09, 19), EndDate = new DateTime(2022, 09, 19), GraduationDate = new DateTime(2022, 09, 19) };
         
         //Act
-        var expectedStatus = Status.Graduated; 
-        var actualStatus = s.Status;
+        var expected = Status.Graduated; 
+        var actual = s.Status;
 
         //Assert
-        expectedStatus.Should().Be(actualStatus);
-
+        actual.Should().Be(expected);
     }
 
     [Fact]
@@ -68,11 +66,10 @@ public class StudentTests
         Student s = new Student { Id = 1, GivenName = "Joe", SurName = "Exotic", StartDate = new DateTime(2020, 09, 19), EndDate = new DateTime(2022, 09, 19), GraduationDate = new DateTime(2022, 09, 19) };
         
         //Act
-        var expectedStatus = s.ToString(); 
-        var actualStatus = "Id: 1, Given name: Joe, Surname: Exotic, Status: Graduated, Start date: 19-09-2020 00:00:00, End date: 19-09-2022 00:00:00, Graduation date: 19-09-2022 00:00:00";
+        var expected = "Id: 1, Given name: Joe, Surname: Exotic, Status: Graduated, Start date: 19-09-2020 00:00:00, End date: 19-09-2022 00:00:00, Graduation date: 19-09-2022 00:00:00";
+        var actual = s.ToString();
 
         //Assert
-        expectedStatus.Should().Be(actualStatus);
-
+        actual.Should().Be(expected);
     }
 }
