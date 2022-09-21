547aa5908e84afb5a5f0c95b3f094450ab9cc4d3
Mathias Hvolgaard Darling Larsen <mhvl@itu.dk>
2022-09-21 16:03:27 +0200

    queries and tests initialized

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,8 +3,18 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void WizardsInventedByRowlingGivenListOfWizardsReturnsListOfWizardNames()
     {
+        var wizards = WizardCollection.Create();
 
+        List<string> expected = new()
+        {
+            "Harry Potter",
+            "Voldemort",
+            "Albus Dumbledore"
+        };
+
+        var actual = Queries.WizardsInventedByRowling(wizards);
+        actual.Should().BeEquivalentTo(expected);
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,11 @@ namespace Assignment2;
 
 public class Queries
 {
+    public static IEnumerable<string> WizardsInventedByRowling(IEnumerable<Wizard> wizards)
+    {
+        return from w in wizards
+               where w.Creator is "%Rowling%"
+               select w.Name;
+
+    }
 }
